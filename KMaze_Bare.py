67cc5f36e0eb2a8bad86cdad4b12af6d961b068e
PaulFMMuller <paul.fm.muller@gmail.com>
2018-05-02 22:52:35 -0500

    Revert "KMaze augmented ; UVFA Started"
    This reverts commit 6cad46610e2e9698c4cef6becb9172e4847c01d8.

--- KMaze_Bare.py
+++ KMaze_Bare.py
@@ -10,7 +10,7 @@ import numpy as np
 
 class KMaze_Bare:
     
-    def __init__(self,N,K,chosenDim,initPos=None,random_seed=None,minReward=1/1000,maxReward=1):
+    def __init__(self,N,K,chosenDim,initPos=None,random_seed=0,minReward=1/1000,maxReward=1):
         if (not initPos is None) and (not (len(initPos) == K)):
             raise Exception('Error : initial position array and dimensional number mismatch : K = {} ; len(initPos) = {}'.format(\
                             K,len(initPos)))
@@ -32,26 +32,19 @@ class KMaze_Bare:
         
         
     def render(self):
-        # Only one dimension : we plot a line.
         if   self.K == 1:
             outputString = '.' * self.N
             outputString = list(outputString)
             outputString[self.agentPos[0]] = '0'
             outputString = ''.join(outputString)
-        # Two dimensions : we plot a grid.
         elif self.K == 2:
             newStrings = []
             outputString = ''
             for i in range(self.N):
                 newStrings.append('.' * self.N)
-            
-            # If the current position is still within the cube, we plot
-            # the agent.
-            if self.agentPos[0] >= 0 and self.agentPos[0] < self.N and\
-                self.agentPos[1] >= 0 and self.agentPos[1] < self.N:
-                tempString = list(newStrings[self.agentPos[0]])
-                tempString[self.agentPos[1]] = '0'
-                newStrings[self.agentPos[0]] = ''.join(tempString)
+            tempString = list(newStrings[self.agentPos[0]])
+            tempString[self.agentPos[1]] = '0'
+            newStrings[self.agentPos[0]] = ''.join(tempString)
             for i in range(self.N):
                 outputString += newStrings[i] + '\n'
         else:
@@ -59,12 +52,10 @@ class KMaze_Bare:
         print(outputString)
         
         
-    # Sample a random action from the environment.
     def sample(self):
         return np.random.randint(low=0,high=2*self.K)
     
     
-    # Resets the environment without changing the start position.
     def reset(self):
         self.agentPos = self.startingPos
         return self.agentPos
@@ -83,52 +74,11 @@ class KMaze_Bare:
         reward = 0                              # 0 by default
         info   = [None]
         done   = False
-        if np.any(self.agentPos < 0) or np.any(self.agentPos >= self.N):
+        if np.any(self.agentPos <= 0) or np.any(self.agentPos >= self.N):
             done = True
-            dim = self.chosenDim // 2
-            pos = self.chosenDim - 2*dim
-            if (self.agentPos[dim] < 0 and pos == 0) or (self.agentPos[dim] >= self.N and pos == 1):
+            if self.agentPos[self.chosenDim] >= self.N:
                 reward = self.maxReward
             else:
                 reward = self.minReward
             
         return observation, reward, done, info
-    
-    
-    # Used at the end of training to find the goal reached.
-    def getHERGoal(self):
-        i = 0
-        while self.agentPos[i] >= 0 and self.agentPos[i] < self.N:
-            i = i+1
-        K = 2*i
-        if self.agentPos[i] >= self.N:
-            K = K+1
-        return K
-        
-    
-    # Used for Supervised Learning Training
-    def getQMatrix(self,gamma):
-        matrixDim = [self.N]*self.K
-        resMatrix = np.zeros(matrixDim)
-        
-        dim = self.chosenDim // 2
-        pos = self.chosenDim - 2*dim
-        
-        for index, x in np.ndenumerate(resMatrix):
-            closestDim = min(np.min(index),self.N-1-np.max(index))
-            chosenDim  = index[dim]
-            if pos == 1:
-                chosenDim = self.N-1-chosenDim
-            if chosenDim <= closestDim - (np.log(self.maxReward/self.minReward)/np.log(gamma)):
-                resMatrix[index] = self.maxReward * (gamma**chosenDim)
-            else:
-                resMatrix[index] = self.minReward * (gamma**closestDim)
-        return resMatrix
-        
-        
-        
-        
-        
-        
-        
-        
