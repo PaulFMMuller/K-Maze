db92894151fb4df1cd5717c5c4c8497db4666b6d
PaulFMMuller <paul.fm.muller@gmail.com>
2018-05-03 23:06:11 -0500

    UVFA Implemented
    UVFA Implemented with Matrix factorization interpolated through an MLP,
    and h function : dot product.
    
    Still to be tested, absence of bugs unguaranteed.

--- UVFA.py
+++ UVFA.py
@@ -5,7 +5,11 @@ Created on Sun Apr 29 12:25:22 2018
 @author: Paul
 """
 
+from sklearn.model_selection import train_test_split
 from sklearn.decomposition import NMF
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Input,Dense
+from keras.models import Model,load_model
 import numpy as np
 
 
@@ -28,8 +32,68 @@ class UVFA:
             mat = np.concatenate((mat,QValueMatrixes[i].reshape(-1,1)),axis=1)
         
         W = self.NMF.fit_transform(mat)
-        H = self.NMF.components_
+        H = np.transpose(self.NMF.components_)
+        
+        self.WNetwork = self.createNeuralNetwork(W.shape[1])
+        self.HNetwork = self.createNeuralNetwork(H.shape[1])
+        
+        Wtrain,Wtest,Strain,Stest = train_test_split(W,states,test_size=0.2)
+        Htrain,Htest,Gtrain,Gtest = train_test_split(H,g,test_size=0.2)
+        
+        # Use of an ensemble of NNs should be better. To be investigated.
+        callBack = ModelCheckpoint('tempModel.mod', monitor='val_loss', save_best_only=True)
+        self.WNetwork.fit(Strain,Wtrain,batch_size=32,validation_data=(Stest,Wtest),callbacks=[callBack])
+        self.WNetwork = load_model('tempModel.mod')
+        
+        self.HNetwork.fit(Gtrain,Htrain,batch_size=32,validation_data=(Gtest,Htest),callbacks=[callBack])
+        self.HNetwork = load_model('tempModel.mod')
+
+        
+    def createNeuralNetwork(self,inputShape):
+        inputVar = Input(shape=(self.inputShape,))
+        
+        h = Dense(self.inputShape,activation='relu')(inputVar)
+        for i in range(self.n_layers-2):
+            h = h = Dense(self.inputShape,activation='relu')(h)
+        res = Dense(self.rank,activation='linear')(h)
+        
+        modele = Model(inputs = inputVar,outputs=res)
+        modele.compile(optimizer='Adam',loss='mean_squared_error',metric='mean_squared_error')
+        return modele
         
         
     def predict(self,state,g):
-        pass
+        Phi = self.WNetwork.predict(state)
+        Psi = self.HNetwork.predict(g)
+        
+        return (self.h(Phi,Psi))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
